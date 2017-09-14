00e55bcd6ffce0966481c0db76b9eb2643eae7d4
Leandro <leandrotrovoes@hotmail.com>
2017-09-14 16:06:28 -0300

    add request lib

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 const config  = require("./config.js");
 const logger  = require("./logger.js");
+const request = require("request");
 
 const client = new Discord.Client();
 const token = config.token;
