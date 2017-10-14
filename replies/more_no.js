21590688d44b09247aacbec39215725c0141a7aa
Leandro <leandrotrovoes@hotmail.com>
2017-10-13 22:23:11 -0300

    new responses 13oct17

--- replies/more_no.js
+++ replies/more_no.js
@@ -3,5 +3,17 @@ module.exports = [
     "Oh what's that? I can't understand your horrible punctuation.",
     "I really hope your grammar teacher isn't seeing this.",
     "Fun fact: I was programmed to not answer your primitive word usage.",
-    "English modahfuckah, do you speak it?"
+    "English modahfuckah, do you speak it?",
+    "Did you know that 4/5 doctors recommend proper grammar? The last one is actually a dentist!",
+    "ERROR: Question not found.",
+    "Nani!?",
+    "IDIOT WARNING!! We got an idiot over here!",
+    "So that's the result of dropping school?",
+    "I apologize my good sir, but your language does not seem to suit the criteria required for I, the 8 Bot, to accordingly reward you with a proper response.",
+    "Bitch are you even trying to spell?",
+    "Spell with me; Q-U-E-S-T-I-O-N M-A-R-K.",
+    "I'll have you know that my creator works for Discord and your lack of basic grammar triggers me.",
+    "What do you and a banana have in common? They both can't ask questions.",
+    "QUESTION ME YOU FILTHY ANIMAL",
+    "Watch your language, children are seeing this."
 ]
--- replies/none.js
+++ replies/none.js
@@ -6,5 +6,10 @@ module.exports = [
     "Yes, yes I do work, now try asking me something.",
     "You called?",
     "Yes, I am here, I was created to be here, now if you can just move on and ask me something instead of waiting to see my response. That'd be great.",
-    "Just saying, but if you typed \"8ball\", what disability kept you from typing further?"
+    "Just saying, but if you typed \"8ball\", what disability kept you from typing further?",
+    "Have you tried making a question? I think you'll love it!",
+    "Type \"8ball --help\" for help, if you just wanted me to be here for you, you could use some friends.",
+    "I was not created for company, get yourself a Waifu Bot.",
+    "Never fear, 8 Bot is here!",
+    "Roses are red, Violets are blue, You didn't ask me anything, So I shall not answer you."
 ]
